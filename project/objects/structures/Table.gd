2023138d3923799b856280acdb9f82b3ea3da0fd
IvessJohn <59248545+IvessJohn@users.noreply.github.com>
2021-08-28 17:43:25 +0300

    Flipping tables added (1.0)
    The state of the project as of by the end of the livestream on 28.08.2021

--- project/objects/structures/Table.gd
+++ project/objects/structures/Table.gd
@@ -1,12 +1,27 @@
 extends "res://project/objects/structures/StructureBase.gd"
 
 
+var flipped: bool = false
+
+export(AudioStream) var SFX_FLIP: AudioStream = null
+
 onready var flipTween = $FlipTween
 
 
+func _physics_process(delta):
+	if Input.is_action_just_pressed("action"):
+		if !flipped:
+			flip()
+
 func flip():
+	flipped = true
 	hurtbox.set_collisionShape_disabled(false)
 	
-	var target_position = global_position
+	var flip_direction = 1 # Right
 	
-	flipTween.interpolate_property(self)
+#	var target_position = global_position +
+	var target_rot_degrees = 90 * flip_direction
+	
+	flipTween.interpolate_property(self, "rotation_degrees", rotation_degrees,
+			target_rot_degrees, 0.125, Tween.TRANS_BACK, Tween.EASE_IN_OUT)
+	flipTween.start()
